a7f8a3a2f09393c0a5fd1b7f4d0a14e4ddb3d883
sandeepkrishnagopalappa <75685782+sandeepkrishnagopalappa@users.noreply.github.com>
2021-03-17 22:33:59 +0530

    Update web_utility.py

--- Library/web_utility.py
+++ Library/web_utility.py
@@ -9,6 +9,12 @@ class Generic_method:
         locator_type,locator_value=locator
         driver.find_element(locator_type,locator_value).send_keys(str(values))
 
+    def selected(self, driver, locator):
+        locator_type, locator_value = locator
+        return driver.find_element(locator_type, locator_value).is_selected
 
+    def displayed(sel,driver,locator):
+        locator_type, locator_value = locator
+        return driver.find_element(locator_type, locator_value).is_displayed
 
 
