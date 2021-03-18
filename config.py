e00815afe022044d520cb4b37a4ed0502dcfcb8b
sandeepkrishnagopalappa <75685782+sandeepkrishnagopalappa@users.noreply.github.com>
2021-03-18 15:59:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ executable_path=r"C:\Users\7000026201\Downloads\chromedriver_win32\chromedriver.
 url="https://www.irctc.co.in/nget/train-search"
 file_name=r"C:\Users\7000026201\Documents\Tasks_Folder\register1.xls"
 sheet_name= 'object'
-flight_title="IRCTC Next Generation eTicketing System"
+expected_irctc_page_title="FlightTicketBooking Next Generation eTicketing System"
+expected_flight_page_title="Air Ticket Booking | Book Flight Tickets | Cheap Air Fare - FlightTicketBooking Air"
