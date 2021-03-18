9de5e41d52263fedf70a1e1281b2c4114c0b6f35
sandeepkrishnagopalappa <75685782+sandeepkrishnagopalappa@users.noreply.github.com>
2021-03-18 15:56:28 +0530

    Update web_utility.py

--- Library/web_utility.py
+++ Library/web_utility.py
@@ -1,20 +1,22 @@
 
+
 class Generic_method:
 
-    def click(self,driver,locator):
+    def click_on_element(self,driver,locator):
         locator_type,locator_value=locator
         driver.find_element(locator_type,locator_value).click()
 
-    def send_keys(self,driver,locator,*,values):
+    def enter_text(self,driver,locator,*,values):
         locator_type,locator_value=locator
         driver.find_element(locator_type,locator_value).send_keys(str(values))
 
-    def selected(self, driver, locator):
+    def validate_check_radio_button(self,driver,locator):
         locator_type, locator_value = locator
-        return driver.find_element(locator_type, locator_value).is_selected
+        return driver.find_element(locator_type, locator_value).is_selected()
 
-    def displayed(sel,driver,locator):
+    def validate_element_display(sel,driver,locator):
         locator_type, locator_value = locator
-        return driver.find_element(locator_type, locator_value).is_displayed
+        return driver.find_element(locator_type, locator_value).is_displayed()
 
-
+    def get_page_title(self, driver):
+        return driver.title
